2466765b1abef10bbd1289060c4104f024f42f57
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-10-12 23:32:22 +0800

    feat: adjust angle of egg splat based on angle of collision

--- Assets/Scripts/Logic/President/DestroyOnTrigger.cs
+++ Assets/Scripts/Logic/President/DestroyOnTrigger.cs
@@ -9,9 +9,19 @@ public class DestroyOnTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
-        if (other.gameObject == target) {
-            GameObject obj = Instantiate(splatter, rb.position, Quaternion.identity);
+        if (other.gameObject == target)
+        {
+            Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+            Vector2 collisionPoint = rb.position;
+            
+            // Get the closest point on the collider to find the surface normal
+            Vector2 closestPoint = other.ClosestPoint(collisionPoint);
+            Vector2 surfaceNormal = (closestPoint - collisionPoint).normalized;
+            
+            float angle = Mathf.Atan2(surfaceNormal.y, surfaceNormal.x) * Mathf.Rad2Deg;
+
+            GameObject obj = Instantiate(splatter, collisionPoint, Quaternion.Euler(0, 0, angle + 90));
+
             obj.SetActive(true);
             Destroy(gameObject);
         }
