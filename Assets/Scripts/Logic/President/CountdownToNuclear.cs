e19412397d48da6cc7ac7fd57e2bda6a92719acd
ThePlasmak <59949361+ThePlasmak@users.noreply.github.com>
2024-10-12 23:31:30 +0800

    feat: increase length of president scene slightly

--- Assets/Scripts/Logic/President/CountdownToNuclear.cs
+++ Assets/Scripts/Logic/President/CountdownToNuclear.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CountdownToNuclear : MonoBehaviour
 {
-    public float countdownTime = 20.0f; 
+    public float countdownTime = 23.0f; 
     private float elapsedTime = 0.0f;   
     public string nextScene;
     
